3bcd31cb61430d9d7ce3596da85b82d2c127bd86
JustInTimez <just.oost@gmail.com>
2022-12-04 20:36:43 +0200

    Class update
    For users to register to the site.

--- data/userDAO.php
+++ data/userDAO.php
@@ -23,10 +23,50 @@ class UserDAO {
         return $user;
     }
 
+    public static function createUser() {
+        global $connect;
+        $fname = trim($_POST['RegInputName']);
+        $lname = trim($_POST['RegInputSurname']);
+        $email = trim($_POST['RegInputEmail']);
+        $password = trim($_POST['RegInputPassword']);
+
+        // Begin prepare statement
+        $sql = "INSERT INTO users (fname, lname, email, password) VALUES (?, ?, ?, ?)";
+        $stmt = $connect->prepare($sql);
+        $stmt->bind_param("ssss", $fname, $lname, $email, $password);
+        
+        // Check if user was added to db and then redirect appropriately
+        if ($stmt->execute() === TRUE){
+
+            $sql = "SELECT * FROM users WHERE password = '" . $password . "'";
+            $result = $connect->query($sql);
+
+            if ($result->num_rows == 1) {
+                $user = $result->fetch_assoc();
+                $_SESSION['LoggedInUser'] = $user;
+
+                // Close connection
+                mysqli_close($connect);
+                
+                return true;
+            } else {
+                return false;
+            }
+
+        } else {
+
+            echo "Error: " . $sql . "<br>" . $connect->error;
+
+            // Close connection
+            mysqli_close($connect);
+
+            header("Location: ../login.php");
+            exit();
 
 
-
-
+        }
+        
+    }
 
 
 
--- model/User.php
+++ model/User.php
@@ -3,35 +3,50 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
-include __DIR__ . "./../data/userDAO.php";
+include __DIR__ . "/../data/userDAO.php";
 
 class User {
 
     // ========================= FIELDS =========================
 
     private $id;
-    private $name;
-    private $image;
-    private $price;
-    private $description;
-    private $rating;
-    private $genre;
-    private $is_pc;
-    private $is_xbox;
-    private $is_ps;
+    private $fname;
+    private $lname;
+    private $email;
+    private $password;
+    private $contact_no;
+    private $dob;
 
 
-    public function __construct($id){
+    public function __construct($id, $fname, $lname, $email, $password, $contact_no, $dob){
 
         // Call to DAO
-        $user = UserDAO::fetchUser($id);
+        // $stmt = UserDAO::fetchUser($id);
 
+        $this->id = $id;
+        $this->fname = $fname;
+        $this->lname = $lname;
+        $this->email = $email;
+        $this->password = $password;
+        $this->contact_no = $contact_no;
+        $this->dob = $dob;
 
     }
 
     // ========================= METHODS =========================
 
- 
+    public static function userRegister() {
+        $result = UserDAO::createUser();
+        
+        if ($result == true) {
+            
+        echo "New user created successfully";
+
+        header("Location: ../index.php");
+        exit();
+
+        }
+    }
 
 
 
