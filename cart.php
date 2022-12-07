6936a309a0ea4f12235a66c5566ab1cb434a4ee2
JustInTimez <just.oost@gmail.com>
2022-12-07 14:23:40 +0200

    Adjustments
    Made to shop, some class functions and added HTML for output of SESSION Cart - struggling to get the right loop to work on output

--- cart.php
+++ cart.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<?php include __DIR__ . "/partials/header.php";
-
+<?php
+include __DIR__ . "/partials/header.php";
+include __DIR__ . "/model/Order.php";
 
 // Check if the user is already logged in, if yes then redirect them to homepage
 // if (isset($_SESSION["LoggedInUser"]) && $_SESSION["LoggedInUser"] === true) {
@@ -10,6 +11,7 @@
 //     exit;
 // }
 
+$_SESSION['Cart'];
 ?>
 
 
@@ -24,6 +26,74 @@
         </div>
         <!-- Hero END -->
 
+        <!-- Cart List  -->
+        <div class="table-responsive m-5">
+            <?php if(isset($_SESSION['Cart'])) ?>
+                <?php foreach($_SESSION['Cart'] as $item) ?>
+                    <table class="table table-hover table-responsive-md">
+                        <thead>
+                            <tr>
+                                <th>Order No</th>
+                                <th>Game Name</th>
+                                <th>Platform</th>
+                                <th>Rating</th>
+                                <th>Rating</th>
+                                <th>Genre</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody class="table-group-divider">
+                            <tr>
+                                <td><?= $item->$_SESSION['Cart'][0] ?></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td>R </td>
+                                <td><button type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#cancelModal' . $hotel->id . '">
+                                        Cancel?
+                                    </button></td>
+
+                                <!-- Modal -->
+                                <div class="modal fade" id="cancelModal' . $hotel->id . '" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <form action="./processing/" method="post">
+                                                <div class="modal-header">
+                                                    <h1 class="modal-title fs-5">ADD NAME HERE</h1>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <img src="./static/images/products/' ADD IMAGE HERE FOR FILE PATH '" height="270" class="card-img-top product-image" alt="ADD NAME HERE">
+                                                    <div>
+                                                        <p class="fs-3">Want to Refund?</p>
+
+                                                        <p class="fs-6">Please note that refunds can only be done within <i>24hrs (1 day) after a successful purchase</i>.<br><br>
+                                                            Should you choose to cancel, we will check your validity and inform you.
+                                                        </p>
+                                                        <p class="fs-5 text-center">Are you sure you\'d like to cancel?</p>
+                                                        <input type="hidden" name="productId" value="ADD PRODUCT ID HERE">
+                                                    </div>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                    <button type="submit" name="confirmCancel" class="btn btn-dark">Yes, please refund me?</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </tr>
+                        </tbody>
+                    </table>
+                <?php endforeach ?>
+            <?php else : ?>
+                <h2>Sorry, there's nothing in yo cart ATM!</h2>
+            <?php endif ?>
+
+
+        </div>
     </main>
 
     <?php include __DIR__ . "/partials/footer.php"; ?>
--- contact.php
+++ contact.php
@@ -15,6 +15,7 @@
 
 <body>
     <main>
+
         <!-- Hero -->
         <div class="jumbotron jumbotron-fluid text-center bg-light d-flex align-items-center justify-content-center">
             <div class="container mt-5">
@@ -24,6 +25,10 @@
         </div>
         <!-- Hero END -->
 
+        <h2 class="text-center my-5">Wanna visit us?</h2>
+        <div class="maps-container">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3312.6993033604444!2d18.63279031525808!3d-33.871639526637644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc50b0ac0a214f%3A0x66973f0ada04f5e1!2sBattle%20Bunker!5e0!3m2!1sen!2sza!4v1670333667516!5m2!1sen!2sza" width="1200" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
     </main>
 
     <?php include __DIR__ . "/partials/footer.php"; ?>
